ca6fee74005fa56cf05e24f0d23ff9adc984d668
Andrey Morozov <alt-j@yandex-team.ru>
2016-08-30 17:17:45 +0300

    add mixins example

--- examples/mixin.js
+++ examples/mixin.js
@@ -1,1 +1,29 @@
-// to be continued ...
+var React = require('../src/index');
+
+// All declarations must be transformed from jsx to js.
+// Also you must remove all propTypes (in case of babel, you can use transform-react-remove-prop-types).
+var ComponentBase = {
+    renderText: function () {
+        return React.createElement('div', {
+            className: 'text',
+            dangerouslySetInnerHTML: {__html: this.props.text}
+        });
+    }
+};
+
+var Component = React.createClass({
+    mixins: [ComponentBase],
+
+    getDefaultProps: function () {
+        return {
+            text: 'Some <b>bold</b> text'
+        };
+    },
+
+    render: function () {
+        return React.createElement('div', null, this.renderText());
+    }
+});
+
+var element = React.createElement(Component);
+console.log(React.renderToString(element));
