066c561ae848155bcfcc8c0c36ecd2780e3fb005
Andrey Morozov <alt-j@yandex-team.ru>
2016-09-16 22:52:59 +0300

    improve benchmark command

--- tools/benchmark.js
+++ tools/benchmark.js
@@ -1,10 +1,13 @@
 var run = require('fast-react-benchmark/run');
+var tests = require('fast-react-benchmark/tests');
 
 var ReactRender = require('fast-react-render');
 var ReactServer = require('../src/index');
 
 run({
-    'FastReactServer + FastReactRender': {
+    'FastReactServer + FastReactRender': tests['FastReactServer + FastReactRender'],
+
+    'This + FastReactRender': {
         engine: ReactServer,
         run: function (listView, dataSet) {
             var element = ReactServer.createElement(listView, dataSet);
